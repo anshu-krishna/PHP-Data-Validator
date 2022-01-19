e26beb6a03e2465b100174e8fe7e6a46236ad1bb
Anshu Krishna <anshu.krishna5@gmail.com>
2022-01-20 00:51:44 +0530

    Version 2.2

--- src/Validator.php
+++ src/Validator.php
@@ -4,7 +4,6 @@ namespace Krishna\DataValidator;
 class Validator {
 	public readonly ArrayHandler|ObjectHandler $struct;
 	public function __construct(array|string $data_struct, public readonly bool $trimOutOfBound = false) {
-		$error = [];
 		if(is_array($data_struct)) {
 			$data_struct = json_encode($data_struct, JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_INVALID_UTF8_SUBSTITUTE);
 		}
@@ -12,18 +11,11 @@ class Validator {
 		if($data_struct === null) {
 			throw new MultiLinedException('Invalid structure');
 		}
-		try {
-			$this->struct = match(true) {
-				is_object($data_struct) => new ObjectHandler($data_struct, $this->trimOutOfBound),
-				is_array($data_struct) => new ArrayHandler($data_struct, $this->trimOutOfBound),
-				default => throw new MultiLinedException('Invalid structure')
-			};
-		} catch(MultiLinedException $ex) {
-			$error = $ex->getInfo();
-		}
-		if(count($error) > 0) {
-			throw new MultiLinedException($error);
-		}
+		$this->struct = match(true) {
+			is_object($data_struct) => new ObjectHandler($data_struct, $this->trimOutOfBound),
+			is_array($data_struct) => new ArrayHandler($data_struct, $this->trimOutOfBound),
+			default => throw new MultiLinedException('Invalid structure')
+		};
 	}
 	public function validate(array|object $val) : Returner {
 		$res = $this->struct->validate($val);
