66041ea6b9aeab7eb5d9c456eaad70837af05fb4
Anshu Krishna <anshu.krishna5@gmail.com>
2022-01-27 16:10:37 +0530

    bugfix: string handler

--- src/Types/StringType.php
+++ src/Types/StringType.php
@@ -6,6 +6,9 @@ use Krishna\DataValidator\Returner;
 class StringType implements \Krishna\DataValidator\TypeInterface {
 	use \Krishna\DataValidator\StaticOnlyTrait;
 	const Name = 'string';
+	private static function can_be_string($var) {
+		return $var === null || is_scalar($var) || (is_object($var) && method_exists($var, '__toString'));
+	}
 
 	public static function validate($value, bool $allow_null = false) : Returner {
 		if($allow_null && ($f = NullType::validate($value))->valid) {
@@ -14,7 +17,6 @@ class StringType implements \Krishna\DataValidator\TypeInterface {
 		if(is_string($value)) {
 			return Returner::valid($value);
 		}
-		// return Returner::invalid(static::Name);
-		return Returner::valid(strval($value));
+		return static::can_be_string($value) ? Returner::valid(strval($value)) : Returner::invalid(static::Name);
 	}
 }
